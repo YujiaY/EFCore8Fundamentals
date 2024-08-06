727019d9c41afe541baa0ad5053ecef047680080
Jack Yuan <jack.yujia.yuan@gmail.com>
2024-08-06 22:01:12 +1000

    AuthorAndBookRelationship and no new actual migrations.

--- M05 Migrations/After/PublisherDomain/Book.cs
+++ M05 Migrations/After/PublisherDomain/Book.cs
@@ -6,5 +6,6 @@ public class Book
     public string Title { get; set; }
     public DateOnly PublishDate { get; set; }
     public decimal BasePrice { get; set; }
-    public Author Author { get; set; }
+    public int AuthorId { get; set; }
+    public virtual Author Author { get; set; }
 }
